d9ba91db19c78127ba541129503f61c638d722ef
danikurniadi3 <danikurniadi1999@gmail.com>
2022-09-06 18:55:47 +0700

    first commit

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName:'Foldable Mini Drone',
+        productNumber:'85631',
+        paymentStatus:'due',
+        shipping:'pending'
+
+    },
+    {
+        productName:'Foldable Mini Drone',
+        productNumber:'85631',
+        paymentStatus:'due',
+        shipping:'pending'
+
+    },
+    {
+        productName:'Foldable Mini Drone',
+        productNumber:'85631',
+        paymentStatus:'due',
+        shipping:'pending'
+
+    },
+    {
+        productName:'Foldable Mini Drone',
+        productNumber:'85631',
+        paymentStatus:'due',
+        shipping:'Declined'
+
+    },
+    {
+        productName:'Foldable Mini Drone',
+        productNumber:'85631',
+        paymentStatus:'due',
+        shipping:'pending'
+
+    },
+    {
+        productName:'Foldable Mini Drone',
+        productNumber:'85631',
+        paymentStatus:'due',
+        shipping:'pending'
+
+    }
+]
